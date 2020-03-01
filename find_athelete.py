5bd3749e47520538dca87a5ce7c13dd3b219d5f9
np <nspetrichenko@gmail.com>
2020-03-01 16:40:11 +0300

    Update find_athelete.py

--- find_athelete.py
+++ find_athelete.py
@@ -1,1 +1,146 @@
+"""
+Задание 2
+Напишите модуль find_athlete.py поиска ближайшего к пользователю атлета. 
 
+Логика работы модуля такова:
+
+- запросить идентификатор пользователя;
+
+- если пользователь с таким идентификатором существует в таблице user, 
+то вывести на экран двух атлетов: ближайшего по дате рождения 
+к данному пользователю и ближайшего по росту к данному пользователю;
+
+- если пользователя с таким идентификатором нет, 
+вывести соответствующее сообщение.
+"""
+
+import datetime
+
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+DB_PATH = "sqlite:///sochi_athletes.sqlite3"
+Base = declarative_base()
+
+
+class Athelete(Base):
+    __tablename__ = 'athelete'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    age = sa.Column(sa.Integer)
+    birthdate = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    height = sa.Column(sa.Float)
+    weight = sa.Column(sa.Integer)
+    name = sa.Column(sa.Text)
+    gold_medals = sa.Column(sa.Integer)
+    silver_medals = sa.Column(sa.Integer)
+    bronze_medals = sa.Column(sa.Integer)
+    total_medals = sa.Column(sa.Integer)
+    sport = sa.Column(sa.Text)
+    country = sa.Column(sa.Text)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = sa.Column(sa.String(36), primary_key=True)
+    first_name = sa.Column(sa.Text)
+    last_name = sa.Column(sa.Text)
+    gender = sa.Column(sa.Text)
+    email = sa.Column(sa.Text)
+    birthdate = sa.Column(sa.Text)
+    height = sa.Column(sa.Float)
+
+
+def connect_db():
+    engine = sa.create_engine(DB_PATH)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(engine)
+    return session()
+
+
+def request_data():
+    print("Ищем похожих атлетов")
+    user_id = input("Идентификатор: ")
+    return int(user_id)
+
+
+def convert_str_to_date(date_str):
+    """
+    Конвертирует строку с датой в формате ГГГГ-ММ-ЧЧ в объект  datetime.date
+    """
+    parts = date_str.split("-")
+    date_parts = map(int, parts)
+    date = datetime.date(*date_parts)
+    return date
+
+
+def nearest_by_bd(user, session):
+    """
+    Ищет ближайшего по дате рождения
+    """
+    athletes_list = session.query(Athelete).all()
+    athlete_id_bd = {}
+    for athlete in athletes_list:
+        bd = convert_str_to_date(athlete.birthdate)
+        athlete_id_bd[athlete.id] = bd
+    
+    user_bd = convert_str_to_date(user.birthdate)
+    min_dist = None
+    athlete_id = None
+    athlete_bd = None
+
+    for id_, bd in athlete_id_bd.items():
+        dist = abs(user_bd - bd)
+        if not min_dist or dist < min_dist:
+            min_dist = dist
+            athlete_id = id_
+            athlete_bd = bd
+    
+    return athlete_id, athlete_bd
+
+
+def nearest_by_height(user, session):
+    """
+    Ищет ближайшего по росту
+    """
+    athletes_list = session.query(Athelete).filter(Athelete.height != None).all()
+    atlhete_id_height = {athlete.id: athlete.height for athlete in athletes_list}
+
+    user_height = user.height
+    min_dist = None
+    athlete_id = None
+    athlete_height = None
+
+    for id_, height in atlhete_id_height.items():
+        dist = abs(user_height - height)
+        if not min_dist or dist < min_dist:
+            min_dist = dist
+            athlete_id = id_
+            athlete_height = height
+    
+    return athlete_id, athlete_height
+
+
+def main():
+    session = connect_db()
+    user_id = request_data()
+    user = session.query(User).filter(User.id == user_id).first()
+    if not user:
+        print("Нет похожих")
+    else:
+        bd_athlete, bd = nearest_by_bd(user, session)
+        height_athlete, height = nearest_by_height(user, session)
+        print(
+            "Ближайший по дате рождения: {}, с датой рождения: {}".format(bd_athlete, bd)
+        )
+        print(
+            "Ближайший по росту: {}, с ростом: {}".format(height_athlete, height)
+        )
+
+
+if __name__ == "__main__":
+    main()
