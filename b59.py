2863818719676fcdb657c64b2381843a91f7601d
np <nspetrichenko@gmail.com>
2020-03-12 21:07:09 +0300

    Update b59.py

--- b59.py
+++ b59.py
@@ -1,16 +1,16 @@
 import time
 
-def time_this(NUM_RUNS=10):
+def time_this(RUNS=10):
     def decorator(function):
         def func(*args, **kwargs):
             avg = 0
-            for i in range(NUM_RUNS):
+            for i in range(RUNS):
                 t0 = time.time()
                 function(*args, **kwargs)
                 t1 = time.time()
                 avg += (t1 - t0)
-            avg /= NUM_RUNS
-            print("%s запусков. Среднее время выполнения %s за : %.5f секунд" % (NUM_RUNS, function.__name__, avg))
+            avg /= RUNS
+            print("%s запусков. Среднее время выполнения %s за : %.5f секунд" % (RUNS, function.__name__, avg))
         return func
 
     return decorator
